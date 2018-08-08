a1fe79801be2338edb639fab350a2e3e7459ef5a
Sreenadh TC <kesav.tc8@gmail.com>
2018-08-08 12:30:48 +0530

    placeholder file for python string lesson

---
+++ 01_Basics/strings.py
@@ -0,0 +1,13 @@
+# Strings in Python
+'''
+We will use this file to write our first string statement in Python
+hello_msg = 'Hello world'
+long_string = "The python string can also be defined using double quotes"
+multiline_string = """The python string also contain multiple lines.
+This is line 1.
+This is line 2.
+By using 'a backward slash' \
+we can tell python to consider what is below as a continuation to \
+the very same line and not to add any tab spacing or new line characters
+And everything is saved onto a single variable"""
+'''
