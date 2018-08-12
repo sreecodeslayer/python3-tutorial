04d2cf0b2aaae727f51eccea8dc126211369b217
Sreenadh TC <kesav.tc8@gmail.com>
2018-08-12 17:48:26 +0530

    Add import statements

---
+++ 01_Basics/imports.py
@@ -0,0 +1,34 @@
+'''
+==============
+Python Import 
+==============
+
+Apart from the functions that we saw up until now, there are
+thousands of functions which are not by default available in Python.
+
+We can import those functionalities using module imports or otherwise known
+as libraries.
+
+Let's see how we can import some of the common modules
+
+`random` module contains many functions for getting randomised outputs,
+like a random number between a range.
+
+`string` module contains many string related functionalities,
+like getting a string of a-z, A-Z, a string containing punctuations and so on
+
+'''
+
+# import random
+# from string import ascii_lowercase
+
+# while True:
+#     inp = input("See an English alphaber?"
+#                 ", Y/N to reply: ")
+#     if inp.lower() == 'y':
+#         print(ascii_lowercase[random.randint(0,25)])
+#     elif inp.lower() == 'n':
+#         break
+#     else:
+#         continue
+
