aa61513e020602b90609910b02d13c3ba0ed2c3c
Sreenadh TC <kesav.tc8@gmail.com>
2018-08-12 12:08:04 +0530

    Basics of user input

---
+++ 01_Basics/inputs.py
@@ -0,0 +1,23 @@
+'''
+======================
+User inputs in Python
+======================
+
+We will use this file to write a simple program that let's user feed data to program
+'''
+
+# print("Hello user,")
+# name = input("What can I call you?\n")
+# age = input("How old are you?\n")
+# print("Hi %s, you are going to be %s next year" % (name, int(age)+1)
+# print("You just earned stars!")
+# print('*'*int(age))
+
+'''
+Well you can go creative with this function, and do many things
+from calculating how long the name is to building a biodata for the user.
+It is all upto you, but to do those we need to learn more.
+
+So once  you have learnt the basics of python, you can come back to this file and 
+add up features to the small program, go crazy.
+'''
