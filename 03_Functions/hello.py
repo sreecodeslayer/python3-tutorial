7f04fb92192d2aeb06667ab6204a7609d94060cb
Sreenadh TC <kesav.tc8@gmail.com>
2018-08-21 13:48:37 +0530

    Functions that return, explain scope

--- 03_Functions/hello.py
+++ 03_Functions/hello.py
@@ -11,16 +11,47 @@ the conditional statements that learned a while ago.
 
 Functions help programmer remove code duplication.
 
+A useless function is like the one below
+
 def function():
     pass
 
 '''
 
 
-def hello(times):
-    '''
-    hello prints out the string 'Hello!' the number of times
-    its asked to line by line.
-    This function takes one required variable (positional argument)
-    '''
-    print('Hello! \n' * times)
+# def hello(times):
+#     '''
+#     hello prints out the string 'Hello!' the number of times
+#     its asked to line by line.
+#     This function takes one required variable (positional argument)
+#     '''
+#     print('Hello! \n' * times)
+
+
+'''
+Funtions that returns an output
+
+This function will however fail with a TypeError if we supply values of types other than
+a number.
+'''
+
+
+# def divide(numerator: float, denominator: float):
+#     try:
+#         return numerator / denominator
+#     except ZeroDivisionError:
+#         print("Cannot divide by zero")
+#         return None
+
+'''
+Scopes and global variables
+
+Look at the function below and guess what might be the output
+before running the code block
+'''
+# def spam():
+#     print(eggs)
+
+# eggs = 42
+# spam()
+# print(eggs)
