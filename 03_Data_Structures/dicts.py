f7fdce13ce9aae61a804a845415d5d54b8a3b0ef
Sreenadh TC <kesav.tc8@gmail.com>
2018-08-24 09:51:33 +0530

    Python Dictionaries

---
+++ 03_Data_Structures/dicts.py
@@ -0,0 +1,6 @@
+'''
+===================
+Python Dictionaries
+===================
+Like a list, a dictionary is a collection of many values. But unlike indexes for lists, indexes for dictionaries can use many different data types, not just integers. Indexes for dictionaries are called keys, and a key with its associated value is called a key-value pair
+'''
